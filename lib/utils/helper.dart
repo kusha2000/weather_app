2463bf4987670c09b79aad8a9a405814a37f24f1
kusha2000 <kushanandarawewa1@gmail.com>
2025-05-05 21:12:17 +0530

    Create helper.dart

---
+++ lib/utils/helper.dart
@@ -0,0 +1,464 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/theme_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).getThemeData,
+      home: HomePage(),
+    );
+  }
+}
